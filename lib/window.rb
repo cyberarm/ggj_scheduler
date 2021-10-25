bcb49ad8ceed953f44f17a030a208c4d3f4ee1cc
Cyberarm <matthewlikesrobots@gmail.com>
2021-10-25 07:52:17 -0500

    Use CyberarmEngine intro state

--- lib/window.rb
+++ lib/window.rb
@@ -1,7 +1,8 @@
 class SchedulerGame
   class Window < CyberarmEngine::Window
     def setup
-      push_state(SchedulerGame::States::MainMenu)
+      push_state(CyberarmEngine::IntroState, forward: SchedulerGame::States::MainMenu)
+      # push_state(SchedulerGame::States::MainMenu)
       # push_state(SchedulerGame::States::GameLost)
       # push_state(SchedulerGame::States::GameWon)
       # push_state(SchedulerGame::States::Game)
@@ -12,7 +13,9 @@ class SchedulerGame
     def button_down(id)
       super
 
-      close if id == Gosu::KB_ESCAPE unless @states.first.is_a?(States::MainMenu)
+      return if @states.first.is_a?(CyberarmEngine::IntroState) || @states.first.is_a?(SchedulerGame::States::MainMenu)
+
+      close if id == Gosu::KB_ESCAPE
     end
   end
 end
